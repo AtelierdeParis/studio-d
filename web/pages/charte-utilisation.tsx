531d435c2bc3fc7a927e2e08aa5272fd6c0e64aa
Guillaume Esnault <mon-compte1131@live.fr>
2021-03-23 20:00:06 +0100

    create page charte

---
+++ web/pages/charte-utilisation.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { SSRConfig } from 'next-i18next'
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { Container, Heading } from '@chakra-ui/react'
+import MarkdownRenderer from '~components/MarkdownRenderer'
+import { ROUTE_USE_POLICY } from '~constants'
+import { Page } from '~@types/page.d'
+import { getPage } from '~api/api'
+
+interface ICharte {
+  page: Page
+}
+
+const Charte = ({ page }: ICharte) => {
+  return (
+    <Container>
+      <Heading
+        as="h1"
+        textStyle="h1"
+        mt={16}
+        mb={12}
+        textAlign="center"
+        maxW="container.sm"
+        mx="auto"
+      >
+        {page.title}
+      </Heading>
+      <MarkdownRenderer>{page.text}</MarkdownRenderer>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<SSRConfig> = async ({
+  locale,
+}) => {
+  const page = await getPage(ROUTE_USE_POLICY).then((res) => res.data)
+
+  return {
+    props: {
+      page,
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  }
+}
+
+export default Charte
