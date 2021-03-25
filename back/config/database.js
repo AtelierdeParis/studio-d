242351fe447b02a95346c4efd33fec1970822b31
Guillaume Esnault <mon-compte1131@live.fr>
2021-03-25 11:52:18 +0100

    parse db url

--- back/config/database.js
+++ back/config/database.js
@@ -1,20 +1,37 @@
+const url = require("url");
+
 module.exports = ({ env }) => {
+  const SSL = env.bool("POSTGRESQL_ADDON_SSL_SELF", false);
+  const settings = {
+    client: "postgres",
+    host: env("POSTGRESQL_ADDON_HOST", "localhost"),
+    port: env.int("POSTGRESQL_ADDON_PORT", 5432),
+    database: env("POSTGRESQL_ADDON_DB", "strapi"),
+    username: env("POSTGRESQL_ADDON_USER", "strapi"),
+    password: env("POSTGRESQL_ADDON_PASSWORD", "strapi"),
+    ssl: SSL && {
+      rejectUnauthorized: SSL,
+    },
+  };
+
+  if (process.env.DATABASE_URL) {
+    const parsed = url.parse(process.env.DATABASE_URL, true);
+    const [username, password] = parsed.auth.split(":");
+
+    settings.host = parsed.hostname;
+    settings.port = Number(parsed.port);
+    settings.database = parsed.pathname.substr(1);
+    settings.username = username;
+    settings.password = password;
+    settings.ssl = true;
+  }
+
   return {
     defaultConnection: "default",
     connections: {
       default: {
         connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host: env("POSTGRESQL_ADDON_HOST", "localhost"),
-          port: env.int("POSTGRESQL_ADDON_PORT", 5432),
-          database: env("POSTGRESQL_ADDON_DB", "strapi"),
-          username: env("POSTGRESQL_ADDON_USER", "strapi"),
-          password: env("POSTGRESQL_ADDON_PASSWORD", "strapi"),
-          ssl: {
-            rejectUnauthorized: env.bool("POSTGRESQL_ADDON_SSL_SELF", false),
-          },
-        },
+        settings,
         options: {
           autoMigration: true,
         },
