b767721daf79b36b827654d84c808661ed4d8d3d
Guillaume Esnault <mon-compte1131@live.fr>
2021-03-25 16:23:06 +0100

    add env var BASE_URL

--- back/config/server.js
+++ back/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e9d56482165190f33412982e7373c475'),
+      secret: env("ADMIN_JWT_SECRET", "e9d56482165190f33412982e7373c475"),
     },
   },
+  url: env("BASE_URL", "/"),
 });
