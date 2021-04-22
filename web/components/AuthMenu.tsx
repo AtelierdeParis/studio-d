f4d481f33d95faeb098f05f34e8a63fce61bf824
Guillaume Esnault <mon-compte1131@live.fr>
2021-04-22 18:46:39 +0200

    notif in header

--- web/components/AuthMenu.tsx
+++ web/components/AuthMenu.tsx
@@ -1,62 +1,21 @@
-import React, { useMemo } from 'react'
-import { ButtonGroup, Button, Text, Flex, Box } from '@chakra-ui/react'
+import React from 'react'
+import { ButtonGroup, Button } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
 import Link from '~components/Link'
+import AuthenticatedMenu from '~components/AuthenticatedMenu'
 import SigninModal from '~components/Signin/SigninModal'
-import {
-  ROUTE_SIGNUP,
-  ROUTE_ACCOUNT,
-  ROUTE_ACCOUNT_PLACES,
-  ROUTE_ACCOUNT_REQUEST,
-} from '~constants'
-import Squares from 'public/assets/img/squares.svg'
 import { useCurrentUser } from '~hooks/useCurrentUser'
-import { useMyBookings } from '~hooks/useMyBookings'
+import { ROUTE_SIGNUP } from '~constants'
+
 interface IAuthMenu {
   colorMode: 'white' | 'black'
 }
 const AuthMenu = ({ colorMode }: IAuthMenu) => {
   const { t } = useTranslation('common')
   const { data: user } = useCurrentUser()
-  const { data: bookings } = useMyBookings('all', { enabled: Boolean(user) })
-
-  const routeToRedirect = useMemo(() => {
-    if (!user) return null
-    if (user.type === 'place' && user.espaces.length > 0)
-      return ROUTE_ACCOUNT_PLACES
-    if (user.type === 'company' && bookings && bookings.length > 0)
-      return ROUTE_ACCOUNT_REQUEST
-    return ROUTE_ACCOUNT
-  }, [user])
 
   if (user) {
-    return (
-      <Flex
-        as={Link}
-        href={routeToRedirect}
-        alignItems="center"
-        role="group"
-        _hover={{ textDecoration: 'none' }}
-      >
-        <Squares stroke={colorMode} />
-        <Box maxW="15rem">
-          <Text
-            ml={3}
-            pt="2px"
-            fontWeight="500"
-            lineHeight={1.2}
-            isTruncated
-            borderBottom="1px solid transparent"
-            _groupHover={{
-              textDecoration: 'none',
-              borderBottom: `1px solid ${colorMode}`,
-            }}
-          >
-            {user.structureName}
-          </Text>
-        </Box>
-      </Flex>
-    )
+    return <AuthenticatedMenu user={user} colorMode={colorMode} />
   }
 
   return (
---
+++ web/components/AuthenticatedMenu.tsx
@@ -0,0 +1,62 @@
+import React, { useMemo } from 'react'
+import { Text, Flex, Box, Circle } from '@chakra-ui/react'
+import Link from '~components/Link'
+import Squares from 'public/assets/img/squares.svg'
+import { useMyNotifications } from '~hooks/useMyNotifications'
+import { useMyBookings } from '~hooks/useMyBookings'
+import {
+  ROUTE_ACCOUNT,
+  ROUTE_ACCOUNT_PLACES,
+  ROUTE_ACCOUNT_REQUEST,
+} from '~constants'
+
+const AuthenticatedMenu = ({ user, colorMode }) => {
+  const { data: notifs } = useMyNotifications()
+  const { data: bookings } = useMyBookings('all')
+
+  const routeToRedirect = useMemo(() => {
+    if (!user) return null
+    if (user.type === 'place' && user.espaces.length > 0)
+      return ROUTE_ACCOUNT_PLACES
+    if (user.type === 'company' && bookings && bookings.length > 0)
+      return ROUTE_ACCOUNT_REQUEST
+    return ROUTE_ACCOUNT
+  }, [user])
+
+  return (
+    <Flex
+      as={Link}
+      href={routeToRedirect}
+      alignItems="center"
+      role="group"
+      _hover={{ textDecoration: 'none' }}
+    >
+      <Squares stroke={colorMode} />
+      <Box maxW="15rem" pos="relative">
+        <Text
+          ml={3}
+          pt="2px"
+          fontWeight="500"
+          lineHeight={1.2}
+          isTruncated
+          borderBottom="1px solid transparent"
+          _groupHover={{
+            textDecoration: 'none',
+            borderBottom: `1px solid ${colorMode}`,
+          }}
+        >
+          {user.structureName}
+        </Text>
+        <Circle
+          pos="absolute"
+          bgColor="orange.500"
+          size="8px"
+          right="-8px"
+          top="0"
+        />
+      </Box>
+    </Flex>
+  )
+}
+
+export default AuthenticatedMenu
