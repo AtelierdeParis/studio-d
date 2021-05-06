4275d1cc0f8128a08a571cbb51da2a45ac65fca3
Guillaume Esnault <mon-compte1131@live.fr>
2021-05-06 11:11:08 +0200

    regex url in migrate script

--- back/scripts/migrate_user.js
+++ back/scripts/migrate_user.js
@@ -180,7 +180,18 @@ const start = async () => {
               user.phone = row.phone_number;
             }
 
-            user.website = row["_Site internet"];
+            if (row["_Site internet"]) {
+              const match = row["_Site internet"].match(
+                /(https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+              );
+              if (!match) {
+                user.website = "";
+              } else {
+                user.website = row["_Site internet"];
+              }
+            } else {
+              user.website = "";
+            }
 
             const socialReason =
               row[
